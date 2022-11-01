b2454e72b7d007fd13e5b823e706b24645021f1c
ZKLegend <91932089+ZKLegend@users.noreply.github.com>
2022-11-01 23:12:52 +0700

    Delete Bai 1 directory

--- Bai 1/index.js
+++
@@ -1,6 +0,0 @@
-function printWord() {
-    let p = document.createElement('p');
-    p.innerText = "Hello World";
-    let body = document.getElementsByTagName('body')[0];
-    body.appendChild(p);
-}
